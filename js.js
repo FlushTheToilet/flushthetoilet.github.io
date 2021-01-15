acfdaaeb0f5a620b115c510c58bcdec6f8b67367
FlushTheToilet <76953317+FlushTheToilet@users.noreply.github.com>
2021-01-15 23:48:11 +0100

    Add files via upload

--- js.js
+++ js.js
@@ -2,12 +2,74 @@ var objet = "none";
 var moves = 0;
 var score = 0;
 var c = 0;
+var m = 0;
 var id_balais = 1;
 var items = document.getElementsByClassName("items");
 
-document.getElementById('idle_music').volume = 0.4;
+// Volume musique
+document.getElementById('idle_music').volume = 0.30;
 
-// Ces tableaux permettent de mettre en image les différents volumes de produit dans le pulvérisateur
+// Tableau pour le monologue
+var tabl_monolog = ['Bien le bonjour et bienvenue dans les toilettes de mon humble établissement, bien évidemment.',
+					'Comme vous pouvez le constater, ceux-ci ont été souillés par de vils personnages !',
+					'J\'aimerais pouvoir les nettoyer moi-même mais mon emploi du temps m\'en empêche. M\'accorderiez-vous l\'obligeance de vous en occuper à ma place ?',
+					'Afin de rendre à ces latrines leur traditionnelle lueur, j\'ai moi-même modestement noté des instructions qui vous aideront à réaliser cette désagréable tâche, bien évidemment.',
+					
+					// STEP 1
+					'Je ne sais point quel malotru s\'est amusé à réaliser de telles sottises, mais à vous dire vrai, j\'espère vraiment que le personnel de mon humble établissement saura lui mettre le grappin dessus.',
+					'Mais trêve de mondanités, je m\'égare !',
+					'Pour ce faire, une brosse imbibée est fortement recommandée, bien évidemment.',
+					'Celle-ci s\'obtient de la façon suivante : vous devez vaporiser de la javel et verser du bicarbonate sur la brosse.',
+					'Une fois ceci fait, vous aurez l\'immense privilège de nettoyer le mur de ce que je considère comme ma seconde demeure afin de retirer toute trace de cet acte malgracieux.',
+					'N\'oubliez point d’essorer la brosse dans le seau juste après cela, bien évidemment.',
+					
+					// STEP 2
+					'Veuillez observer l\'acte barbare réalisé par un scélérat de la même trempe que le précédent.',
+					'Cet événement me rappelle une sombre affaire dans laquelle était impliqué feu mon oncle...',
+					'Oh, veuillez excuser mon comportement, je me fourvoie une fois de plus.',
+					'Afin de mener à bien ce baroud contre la mal propreté, vous aurez besoin d\'une pelle, vide bien évidemment, qui vous permettra de récolter une par une ces déjections.',
+					'Une fois justice faite, car nulle bouse devrait se trouver sur le bord d\'un WC, il vous faut les vider dans la poubelle, bien évidemment.',
+
+					// STEP 3
+					'Que cet infâme personnage aille au diable !',
+					'Je ne sais point de quelle façon il est possible de pissoter ainsi, mais il faut vraiment être un poltron pour plier bagage sans prévenir la réception.',
+					'La marche à suivre pour éliminer ces impuretés est accessible à toute gent, qu\'elle fasse partie de ma caste ou bien de celle comparable aux plébéiens.',
+					'À cet égard, une éponge imbibée de javel est nécessaire pour les traces présentes sur la cuvette et le couvercle des latrines.',
+					'Celles situées au sol requièrent un seau rempli de javel.',
+					'ATTENTION !',
+					'Vous devez nettoyer les traces de la cuvette avant de penser à vous occuper de son nettoyage complet !',
+					'Vous pouvez bien évidemment rincer l\'éponge dans le seau.',
+
+					// STEP 4
+					'Je ne comprends point mes subordonnés... Comment diable peut-on laisser de telles immondices se déployer ainsi ?',
+					'Bon, ressaisissons-nous... Et trêve de bavardage.',
+					'Bien évidemment.',
+					'Avant toute chose, il vous faut remplir le vaporisateur de javel afin d\'en pulvériser sur ces moultes traces d\'humidité.',
+					'Ceci fait, vous pouvez lui rendre sa pureté en passant la brosse dessus.',
+					'Pour celles présentes dans les WC, la brosse de toilettes est nécessaire, bien évidemment.',
+					'Dans le cas où le produit est déposé sur la brosse, il vous est impossible de brosser la moisissure.',
+
+					// STEP 5
+					'Il est impressionnant de voir ce sédiment être présent dans de si improbables recoins.',
+					'De la même façon que l’on nettoie son robinet (dans le cas où celui-ci présente des traces de calcaire, bien évidemment), vous aurez besoin de vinaigre blanc, de bicarbonate, ainsi que d’une brosse.',
+					'Pulvérisez le premier sur les tâches, versez du bicarbonate dessus, puis brossez.',
+					'ATTENTION !',
+					'Le bicarbonate ne peut être versé que dans la cuvette !',
+					'Pour le couvercle et le pied des WC, vous devez le verser sur la brosse, puis brosser (après pulvérisation du vinaigre blanc, bien évidemment).',
+
+					// STEP 6
+					'La dernière étape ! Et probablement la plus simple et intuitive à réaliser, de toute évidence.',
+					'Pour ce faire, versez de la javel dans le seau et amenez le sur le sol maculé.',
+					'Le magnifique balai que nous possédons se chargera du reste.',
+					'J\'ai oublié de vous prévenir, s’il vous reste du vinaigre blanc ou de la javel dans votre vaporisateur, vous devez le vider dans la bouteille du contenu adéquat, bien évidemment.',
+
+					// FIN
+					'Bien. Je pense que cela suffira, bien évidemment.',
+					'In fine, je désire vous apporter ma gratitude pour votre intervention.',
+					'Sans vous, cette salle d\'eau ne saurait retrouver sa jeunesse étincelante.',
+					'Je vous présente mes salutations et vous laisse à vos dépends, bien évidemment, très cher ami.'];
+
+// Ces tableaux qui permettent de mettre en image les différents volumes de produit dans le pulvérisateur
 var tabl_pchit_javel = ['pchit_javel_plein', 'pchit_javel_presque_plein', 'pchit_javel_un_peu_vide', 'pchit_javel_presque_vide', 'pchit_vide'];
 var tabl_pchit_vinaigre = ['pchit_vinaigre_plein', 'pchit_vinaigre_presque_plein', 'pchit_vinaigre_un_peu_vide', 'pchit_vinaigre_presque_vide', 'pchit_vide'];
 
@@ -27,6 +89,26 @@ var audio_sponge2 = new Audio('medias/SOUNDS/sponge2.mp3');
 var audio_sponge3 = new Audio('medias/SOUNDS/sponge3.mp3');
 var audio_verser = new Audio('medias/SOUNDS/verser.mp3');
 var audio_serpilliere = new Audio('medias/SOUNDS/serpilliere.mp3');
+var audio_button_hover = new Audio('medias/SOUNDS/button-up.mp3');
+var audio_button_click = new Audio('medias/SOUNDS/button-down.mp3');
+
+// Ajustement du volume de chaque SFX
+audio_chasse_deau.volume = 0.8;
+audio_bicarbonate.volume = 0.4;
+audio_brosse1.volume = 0.7;
+audio_brosse2.volume = 0.7;
+audio_pchit1.volume = 1;
+audio_pchit2.volume = 1;
+audio_pelle_remplie.volume = 0.4;
+audio_pelle_vidage.volume = 1;
+audio_plouf.volume = 0.3;
+audio_sponge1.volume = 0.6;
+audio_sponge2.volume = 0.6;
+audio_sponge3.volume = 0.6;
+audio_verser.volume = 1;
+audio_serpilliere.volume = 1;
+audio_button_click.volume = 0.18;
+audio_button_hover.volume = 0.18;
 
 // On utilise des tableaux pour les items ayant plusieurs SFX (sélection aléatoire)
 var tabl_audio_brosse = [audio_brosse1, audio_brosse2];
@@ -35,7 +117,7 @@ var tabl_audio_sponge = [audio_sponge1, audio_sponge2, audio_sponge3];
 
 
 function update() {
-	document.getElementById('scoreboard').innerHTML = '<p id="scoreboard">Score : ' + score + ' - - - Mouvements : ' + moves + '</p>';
+	document.getElementById('scoreboard').innerHTML = 'Score : ' + score + '<br> Mouvements : ' + moves;
 }
 update();
 
@@ -359,7 +441,6 @@ function drop(ev) {
 				var anim_balais = setInterval(function() {
 					document.getElementById("balais").src = "medias/ANIMATIONS/balais/balais"+id_balais+".png";
 					id_balais++;
-					console.log(id_balais);
 				}, 100);
 
 				ev.preventDefault();
@@ -765,7 +846,6 @@ function drop(ev) {
 				var anim_balais = setInterval(function() {
 					document.getElementById("balais").src = "medias/ANIMATIONS/balais/balais"+id_balais+".png";
 					id_balais++;
-					console.log(id_balais);
 				}, 100);
 
 				ev.preventDefault();
@@ -1095,4 +1175,101 @@ function drop(ev) {
 	}
 
   	return score;
+}
+
+
+function monolog_prev() {
+	audio_button_click.play();
+	if (m > 0) {
+		m--;
+		document.getElementById("monologue").innerHTML = tabl_monolog[m];
+		
+		if (m<4) {
+			document.getElementById("titre-monologue").innerHTML = "Tutoriel - Partie 0 : Introduction.";
+		}
+		else if (m>3 && m<10) {
+			document.getElementById("titre-monologue").innerHTML = "Tutoriel - Partie 1 : Nettoyer les grafitis.";
+		}
+		else if (m>9 && m<15) {
+			document.getElementById("titre-monologue").innerHTML = "Tutoriel - Partie 2 : Ramasser les excréments.";
+		}
+		else if (m>14 && m<23) {
+			document.getElementById("titre-monologue").innerHTML = "Tutoriel - Partie 3 : Nettoyer l'urine.";
+		}
+		else if (m>22 && m<30) {
+			document.getElementById("titre-monologue").innerHTML = "Tutoriel - Partie 4 : La moisissure.";
+		}
+		else if (m>29 && m<36) {
+			document.getElementById("titre-monologue").innerHTML = "Tutoriel - Partie 5 : Retirer le calcaire.";
+		}
+		else if (m>35 && m<40) {
+			document.getElementById("titre-monologue").innerHTML = "Tutoriel - Partie 6 : Nettoyage du sol.";
+		}
+		else if (m>39) {
+			document.getElementById("titre-monologue").innerHTML = "Tutoriel - Partie 7 : Conclusion.";
+		}
+	}
+	return m;
+}
+function monolog_next() {
+	audio_button_click.play();
+	if (m < tabl_monolog.length-1) {
+		m++;
+		document.getElementById("monologue").innerHTML = tabl_monolog[m];
+		
+		if (m<4) {
+			document.getElementById("titre-monologue").innerHTML = "Tutoriel - Partie 0 : Introduction.";
+		}
+		else if (m>3 && m<10) {
+			document.getElementById("titre-monologue").innerHTML = "Tutoriel - Partie 1 : Nettoyer les grafitis.";
+		}
+		else if (m>9 && m<15) {
+			document.getElementById("titre-monologue").innerHTML = "Tutoriel - Partie 2 : Ramasser les excréments.";
+		}
+		else if (m>14 && m<23) {
+			document.getElementById("titre-monologue").innerHTML = "Tutoriel - Partie 3 : Nettoyer l'urine.";
+		}
+		else if (m>22 && m<30) {
+			document.getElementById("titre-monologue").innerHTML = "Tutoriel - Partie 4 : La moisissure.";
+		}
+		else if (m>29 && m<36) {
+			document.getElementById("titre-monologue").innerHTML = "Tutoriel - Partie 5 : Retirer le calcaire.";
+		}
+		else if (m>35 && m<40) {
+			document.getElementById("titre-monologue").innerHTML = "Tutoriel - Partie 6 : Nettoyage du sol.";
+		}
+		else if (m>39) {
+			document.getElementById("titre-monologue").innerHTML = "Tutoriel - Partie 7 : Conclusion.";
+		}
+	}
+	return m;
+}
+function update_meowzart() {
+	if(m==3 || m==5 || m==7 || m==19 || m==22 || m==24 || m==27 || m==30 || m==35 || m==40 || m==42) {
+		document.getElementById("meowzart").src = "medias/meowzart/meowzart-calme.png";
+	}
+	else if (m==10 || m==15 || m==16) {
+		document.getElementById("meowzart").src = "medias/meowzart/meowzart-colere.png";
+	}
+	else if (m==14 || m==18 || m==26 || m==32 || m==37) {
+		document.getElementById("meowzart").src = "medias/meowzart/meowzart-montre.png";
+	}
+	else if (m==11 || m==23) {
+		document.getElementById("meowzart").src = "medias/meowzart/meowzart-triste.png";
+	}
+	else if (m==0 || m==6 || m==8 || m==12 || m==13 || m==17 || m==25 || m==29 || m==31 || m==36 || m==39 || m==41 || m==43) {
+		document.getElementById("meowzart").src = "medias/meowzart/meowzart-face-calme.png";
+	}
+	else if (m==4 || m==20 || m==33) {
+		document.getElementById("meowzart").src = "medias/meowzart/meowzart-face-colere.png";
+	}
+	else if (m==1 || m==9 || m==21 || m==28 || m==34) {
+		document.getElementById("meowzart").src = "medias/meowzart/meowzart-face-montre.png";
+	}
+	else if (m==2) {
+		document.getElementById("meowzart").src = "medias/meowzart/meowzart-face-triste.png";
+	}
+	else if (m==38) {
+		document.getElementById("meowzart").src = "medias/meowzart/meowzart-sublime.gif";
+	}
 }
